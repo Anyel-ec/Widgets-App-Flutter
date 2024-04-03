75642bcfbc388582f5f2aaf844cbd314b372d4ae
anyel-ec <paulangel826@gmail.com>
2024-04-02 19:17:03 -0500

    theme and style of app

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>  [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.purple,
+  Colors.orange,
+  Colors.yellow,
+];
+
+class AppTheme {
+  final int selectedColor; 
+
+  AppTheme({this.selectedColor = 0}): assert(selectedColor >= 0 && selectedColor < colorList.length);
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+  );
+}
