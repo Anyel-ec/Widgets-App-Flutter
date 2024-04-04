08eddc2d5dc1eb13bdb0ea40c751bfa3f2869f49
anyel-ec <paulangel826@gmail.com>
2024-04-03 19:41:59 -0500

    Add new screen exports, and prepared others widgets

--- lib/presentation/screen/screens.dart
+++ lib/presentation/screen/screens.dart
@@ -1,5 +1,13 @@
+export 'package:widgets_app/presentation/screen/animation/animated_screen.dart';
+export 'package:widgets_app/presentation/screen/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screen/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screen/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screen/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screen/ui_controls/ui_controls_screen.dart';
+
 export 'package:widgets_app/presentation/screen/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screen/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screen/home/home_screen.dart';
 
 // exporto todos los screens para no hacerlo uno por uno  
+
